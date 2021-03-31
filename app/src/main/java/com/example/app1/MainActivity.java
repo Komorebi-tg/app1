22e8d8229cb20cea154fd42d0281001a694a13b3
shiho <78023312+Komorebi-tg@users.noreply.github.com>
2021-03-31 09:14:05 +0900

    commit

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -24,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
-        Button settingsButton = findViewById(R.id.button_setting);
-        Button myProfileButton = (Button)findViewById(R.id.button_my_profile);
-        Button mentalHealthButton = (Button)findViewById(R.id.button_mental_health);
-        Button adviceButton = (Button)findViewById(R.id.button_advice);
+        //Button settingsButton = (Button) findViewById(R.id.button_setting);
+        Button myProfileButton = (Button) findViewById(R.id.button_my_profile);
+        Button mentalHealthButton = (Button) findViewById(R.id.button_mental_health);
+        Button adviceButton = (Button) findViewById(R.id.button_advice);
 
 
 
@@ -40,37 +40,51 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
 
 
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+            }
+        });
+
+ */
+
+/*        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment selectedFragment = new MyProfileFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
             }
         });
 
  */
 
 
-        myProfileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyProfileFragment()).commit();
-            }
-        });
 
-
-        mentalHealthButton.setOnClickListener(new View.OnClickListener() {
+/*        mentalHealthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MentalHealthFragment()).commit();
             }
         });
 
+ */
 
-        adviceButton.setOnClickListener(new View.OnClickListener() {
+
+/*        adviceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AdviceFragment()).commit();
             }
         });
 
+ */
+
     }
+
+    public void onClickButton1(View view) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
+    }
+
+
 
 
 
