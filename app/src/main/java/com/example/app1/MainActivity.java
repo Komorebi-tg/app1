b6276ac3b1fe63b12e700c41095fb46fb089c585
shiho <78023312+Komorebi-tg@users.noreply.github.com>
2021-04-12 16:22:22 +0900

    birth date
    comment out　あります

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -3,8 +3,6 @@ package com.example.app1;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,13 +18,10 @@ import com.example.app1.settings.SettingsNotificationFragment;
 import com.example.app1.settings.SettingsPrivacyFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-
 // test comment2
 public class MainActivity extends AppCompatActivity {
-    TextView tvDate;
-    Button btPickDate;
+    //TextView tvDate;
+    //Button btPickDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,27 +33,29 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
-        tvDate = findViewById(R.id.myProfileDateDisplayBirthDate);
-        btPickDate = findViewById(R.id.birthDatePickerButton);
-        btPickDate.setOnClickListener(new View.OnClickListener() {
-            @Override
+        //tvDate = findViewById(R.id.myProfileDateDisplayBirthDate);
+        //btPickDate = findViewById(R.id.birthDatePickerButton);
+        //btPickDate.setOnClickListener(new View.OnClickListener() {
+            /*@Override
             public void onClick(View v) {
                 com.example.app1.DatePicker mDatePickerDialogFragment;
                 mDatePickerDialogFragment = new com.example.app1.DatePicker();
                 mDatePickerDialogFragment.show(getSupportFragmentManager(), "DATE PICK");
             }
         });
+
+             */
         }
 
-        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        /*public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             Calendar mCalendar = Calendar.getInstance();
             mCalendar.set(Calendar.YEAR, year);
             mCalendar.set(Calendar.MONTH, month);
             mCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
             String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
             tvDate.setText(selectedDate);
-
         }
+         */
 
 
 
